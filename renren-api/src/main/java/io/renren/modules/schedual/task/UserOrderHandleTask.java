82298ac81f7aa32b2ac0ca3a06b68d3bc3832418
xyang4 <18610450436@163.com>
2019-05-16 15:07:11 +0800

    添加注释

--- renren-api/src/main/java/io/renren/modules/schedual/task/UserOrderHandleTask.java
+++ renren-api/src/main/java/io/renren/modules/schedual/task/UserOrderHandleTask.java
@@ -11,9 +11,17 @@ import org.springframework.util.CollectionUtils;
 
 import java.util.Set;
 
+/**
+ * 用户定点处理定时任务<br>
+ * 1 给活跃用户周期地推送可抢订单
+ * 2 考虑订单周期，定期地清理无用的订单
+ * 3 redis 其他数据清理
+ * .
+ * .
+ */
 @Slf4j
 @Component
-public class OrderPushTask {
+public class UserOrderHandleTask {
 
     @Autowired
     IRedisService iRedisService;
