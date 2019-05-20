217670a09b6e394df21324bd3f93c3ad2d2740c2
zhangyipeng <zhangyipeng123>
2019-05-20 14:02:14 +0800

    freemarker等

--- renren-common/src/main/java/io/renren/common/enums/OrdersEntityEnum.java
+++ renren-common/src/main/java/io/renren/common/enums/OrdersEntityEnum.java
@@ -16,6 +16,29 @@ public class OrdersEntityEnum {
         MER_RECHARGE(3,"商户充值"),MER_WITHDROW(4,"商户提现");
         private int value; private String name;
         OrderType(int value,String name){this.value = value;this.name = name;}
+        public static boolean contains(int value) {
+            for (OrderType c : OrderType.values()) {
+                if (c.value==value) { return true; }
+            }
+            return false;
+        }
     }
+    /**
+     * 支付类型：wxqr微信二维码 aliqr支付宝二维码 bank银行卡转账 alitr支付宝转账
+     */
+    @Getter
+    public static enum PayType{
+        WXQR("wxqr","微信二维码"),ALIQR("aliqr","支付宝二维码"),
+        BANK("bank","银行卡转账"),ALITR("alitr","支付宝转账");
+        private String value; private String name;
+        PayType(String value,String name){this.value = value;this.name = name;}
+        public static boolean contains(String value) {
+            for (PayType c : PayType.values()) {
+                if (c.value.equals(value)) { return true;}
+            }
+            return false;
+        }
+    }
+
 
 }
